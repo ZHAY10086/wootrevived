3ef65922ab05b88b186bcf25856180332e4371dd
pharuxtan <28842091+pharuxtan@users.noreply.github.com>
2025-08-28 12:25:54 +0200

    fix(mobs): also remove non-living entities

--- src/main/java/wootrevived/woot/events/InitServer.java
+++ src/main/java/wootrevived/woot/events/InitServer.java
@@ -53,8 +53,10 @@ public class InitServer {
 
                 try {
                     Entity entity = entityType.create(level);
-                    if(!(entity instanceof LivingEntity))
+                    if(!(entity instanceof LivingEntity)) {
+                        WootFactoryMobsRegistry.removeFactoryMob(entityType);
                         continue;
+                    }
 
                     ResourceLocation location = BuiltInRegistries.ENTITY_TYPE.getKey(entityType);
                     mobLocations.add(location);
