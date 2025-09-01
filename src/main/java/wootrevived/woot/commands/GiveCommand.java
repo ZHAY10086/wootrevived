3957d5c1a93dd3dd63f929cd607965699e82ac1a
pharuxtan <28842091+pharuxtan@users.noreply.github.com>
2025-09-01 22:58:29 +0200

    fix(give): don't give absent or blacklisted mobs

--- src/main/java/wootrevived/woot/commands/GiveCommand.java
+++ src/main/java/wootrevived/woot/commands/GiveCommand.java
@@ -55,7 +55,12 @@ public class GiveCommand {
 
     private static int giveItem(CommandSourceStack source, ServerPlayer target, ResourceLocation resourceLocation, CompoundTag tag) {
         EntityType<?> entityType = BuiltInRegistries.ENTITY_TYPE.get(resourceLocation);
+        if(entityType == null || !WootFactoryMobsRegistry.hasFactoryMob(entityType))
+            return 1;
+
         WootFactoryMob<?> mob = WootFactoryMobsRegistry.getFactoryMob(entityType);
+        if(mob.isBlacklisted())
+            return 1;
 
         tag.putString("id", resourceLocation.toString());
 
