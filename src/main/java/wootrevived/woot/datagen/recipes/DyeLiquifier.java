523c4c5c0b1468edd23a55d54322311b18954794
pharuxtan <28842091+pharuxtan@users.noreply.github.com>
2025-10-11 20:03:24 +0200

    revert(balance): revert dye liquifier recipe energy to 500

--- src/main/java/wootrevived/woot/datagen/recipes/DyeLiquifier.java
+++ src/main/java/wootrevived/woot/datagen/recipes/DyeLiquifier.java
@@ -16,7 +16,7 @@ public class DyeLiquifier {
         for (DyeMakeup d : DyeMakeup.values()) {
             DyeLiquifierRecipeBuilder.dyeLiquifierRecipe()
                     .ingredient(Ingredient.of(d.getTag()))
-                    .energy(2500)
+                    .energy(500)
                     .red(d.getRed())
                     .yellow(d.getYellow())
                     .blue(d.getBlue())
@@ -69,7 +69,7 @@ public class DyeLiquifier {
         for (VanillaDyes d : dyes) {
             DyeLiquifierRecipeBuilder.dyeLiquifierRecipe()
                     .ingredient(Ingredient.of(d.item))
-                    .energy(2500)
+                    .energy(500)
                     .red(d.dyeMakeup.getRed())
                     .yellow(d.dyeMakeup.getYellow())
                     .blue(d.dyeMakeup.getBlue())
